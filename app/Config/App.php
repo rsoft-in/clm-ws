c65421464ac9207a840e6f975e9736ebd5484d48
Rajesh Menon <rajeshmenon@rennovationsoftware.com>
2022-05-20 10:14:35 +0530

    config changes

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://192.168.0.2/knoxxbox/ws/';
+    public $baseURL = 'http://localhost/';
 
     /**
      * --------------------------------------------------------------------------
