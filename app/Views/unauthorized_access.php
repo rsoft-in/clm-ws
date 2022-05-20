f73a5479906030a7b924609afb8b26372dbcdb50
Rajesh Menon <rajeshmenon@rennovationsoftware.com>
2022-05-20 23:32:10 +0530

    missed files

---
+++ app/Views/unauthorized_access.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unauthorized Access</title>
+</head>
+
+<body>
+    <div style="font-family:Verdana, Geneva, Tahoma, sans-serif; font-size: 0.9em; padding: 10px; margin-top: 100px; border: 1px solid #cccccc; width: 200px; text-align: center; margin-left: auto; margin-right: auto;">
+        <h3>Unauthorized Access</h3>
+    </div>
+</body>
+
+</html>
